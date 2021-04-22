944d6b0a5997fa51c9ea6d3b8752a18781982933
Кирилл <kirill-titov-2001@mail.ru>
2021-04-22 13:51:36 +0500

    Исправил глючный лабиринт

---
+++ project2/Map.cpp
@@ -0,0 +1,54 @@
+/*
+ * Map.cpp
+ *
+ *  Created on: 21 апр. 2021 г.
+ *      Author:  ирилл
+ */
+
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Map.h"
+
+Map::Map(const char *filename)
+{
+	std::ifstream file;
+	file.exceptions(std::ifstream::badbit);
+	file.open(filename);
+
+	std::string line;
+	std::getline(file, line);
+
+	std::stringstream params_ss(line);
+	params_ss >> _width >> _height >> _start_x >> _start_y >> _start_dir;
+
+	/*	_height		  _width
+	 * 		|			|
+	 * 		v			v
+	 * 	std::vector<std::vector<MapCell>>
+	 */
+
+	_cells.resize(_height);
+	for (int row = 0; row < _height; ++row)
+		_cells[row].resize(_width);
+
+	for (int row = 0; row < _height; ++row) {
+//		_cells[row].resize(_width);
+		std::getline(file, line);
+		for (int col = 0; col < _width; ++col) {
+			switch(line[col]) {
+			case '_':	_cells[col][row] = MapCell::SPACE;	break;
+			case '#':	_cells[col][row] = MapCell::WALL;	break;
+			default:	_cells[col][row] = MapCell::VOIDD;	break;
+			}
+		}
+	}
+}
+
+MapCell Map::cell(int x, int y) const
+{
+	if (x < 0 or y < 0 or x >= _width or y >= _height)
+		return MapCell::VOIDD;
+	return _cells[y][x];
+}
---
+++ project2/Map.h
@@ -0,0 +1,40 @@
+/*
+ * Map.h
+ *
+ *  Created on: 21 апр. 2021 г.
+ *      Author:  ирилл
+ */
+
+#ifndef MAP_H_
+#define MAP_H_
+
+#include <vector>
+
+enum class MapCell
+{
+  VOIDD,
+  SPACE,
+  WALL
+};
+
+class Map final
+{
+protected:
+	int _width { 0 }, _height { 0 };
+	double _start_x { 0. }, _start_y { 0. }, _start_dir { 0. };
+	std::vector<std::vector<MapCell>> _cells;
+public:
+	Map(const char * filename);
+
+	int height() const { return _height; }
+	int width() const  { return _width;  }
+
+	double start_dir() const { return _start_dir; }
+	double start_x() const   { return _start_x; }
+	double start_y() const   { return _start_y; }
+
+	MapCell cell(int x, int y) const;
+
+};
+
+#endif /* MAP_H_ */
